550d8db33406f11498e1ee0f2f57abd717daa099
Dan <dpete016@gmail.com>
2019-04-30 19:50:56 -0400

    database and routes are set up, worked on it with estiven step by step

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(
+  // worked on it with Estiven, used his mLab access to set up connection
+  process.env.MONGODB_URI || "mongodb://estivensal7:Testing123@ds149806.mlab.com:49806/google-books",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
